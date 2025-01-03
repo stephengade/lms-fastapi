d161686d9f32ffa0bac1632e3805a207fbbb88c2
Stephen Gbolagade <gbolabyte222@gmail.com>
2025-01-03 23:55:29 +0100

    fixed psycopg2 error

--- database/db_setup.py
+++ database/db_setup.py
@@ -1,26 +1,13 @@
-
 from typing import Annotated
 from fastapi import Depends
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from sqlmodel import Session, create_engine
 
-# sqlite_file_name = "database.db"
-# sqlite_url = f"sqlite:///{sqlite_file_name}"
-
 APP_DATABASE_URL = "postgresql+psycopg2://user:password@localhost/fast_db"
-CONNECT_ARGS = {}
 
-engine = create_engine(APP_DATABASE_URL, connect_args=CONNECT_ARGS, future=True)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
-
-# Base = declarative_base()
-
+engine = create_engine(APP_DATABASE_URL, future=True)
 
 def get_session():
     with Session(engine) as session:
         yield session
-
 
 SessionDep = Annotated[Session, Depends(get_session)]
